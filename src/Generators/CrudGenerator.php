cba6dbd52179d98737f41ef104c0ab99c458e664
mohammad <usermp76@gmail.com>
2024-06-19 12:05:40 +0330

    Rename class ComponentsGenerator to CrudGenerator

---
+++ src/Generators/CrudGenerator.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Usermp\LaravelGenerator\Generators;
+
+use Symfony\Component\Yaml\Yaml;
+
+class CrudGenerator
+{
+    protected $yamlFile;
+
+    /**
+     * ComponentGenerator constructor.
+     *
+     * @param string $yamlFile
+     */
+    public function __construct($yamlFile)
+    {
+        $this->yamlFile = $yamlFile;
+    }
+
+    /**
+     * Generate the necessary files from the YAML configuration.
+     */
+    public function generate()
+    {
+        $data = Yaml::parseFile($this->yamlFile);
+
+        if( ! $data['service'] ) return null;
+
+        $modelGenerator = new ModelGenerator();
+        $modelGenerator->generateModel($data['service']);
+
+        $controllerGenerator = new ControllerGenerator();
+        $controllerGenerator->generateController($data['service']);
+
+        $controllerGenerator = new RequestGenerator();
+        $controllerGenerator->generateRequests($data['service']);
+    }
+}
